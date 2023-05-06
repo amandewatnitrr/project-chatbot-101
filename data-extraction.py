689e66292b00829030cdfd62862c9cfabbbb1325
amandewatnitrr <amandewatnitrr@gmail.com>
2023-05-06 06:18:37 +0530

    Changes in data-extraction.py

--- data-extraction.py
+++ data-extraction.py
@@ -1,32 +1,27 @@
 import os
 import csv
 
-# define the root directory of the git repository
-root_dir = '/path/to/git/repo'
+root_folder = '/path/to/git/repo'
 
-# define the csv file path to write the results to
-csv_file = '/path/to/result.csv'
+def categorize_files(root_folder):
+    categories = {}
+    for dirpath, dirnames, filenames in os.walk(root_folder):
+        for filename in filenames:
+            full_path = os.path.join(dirpath, filename)
+            category = dirpath.replace(root_folder, '').strip('/')
+            if category not in categories:
+                categories[category] = []
+            categories[category].append((filename, full_path))
 
-# define the column headers for the csv file
-fieldnames = ['file_name', 'category', 'location']
+    return categories
 
-# open the csv file for writing
-with open(csv_file, mode='w', newline='') as csv_file:
-    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+def write_to_csv(categories):
+    with open('file_categories.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['file_name', 'category', 'location'])
+        for category, files in categories.items():
+            for file in files:
+                writer.writerow([file[0], category, file[1]])
 
-    # write the headers to the csv file
-    writer.writeheader()
-
-    # loop through all files and directories in the root directory
-    for root, dirs, files in os.walk(root_dir):
-
-        # loop through all files in the current directory
-        for file in files:
-            # get the full path of the current file
-            file_path = os.path.join(root, file)
-
-            # determine the category based on the file's location
-            category = os.path.relpath(root, root_dir)
-
-            # write the file name, category, and location to the csv file
-            writer.writerow({'file_name': file, 'category': category, 'location': file_path})
+categories = categorize_files(root_folder)
+write_to_csv(categories)
